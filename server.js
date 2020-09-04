33a0a3c9e5021afc17ad0942599b43d9ae156189
Aman Kumar Singh <imh10012.16@bitmesra.ac.in>
2020-09-04 16:10:27 +0530

    Cors added

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
-const api = require('./Routes/endpoints');
-var cors = require('cors');
-
+const api = require("./Routes/endpoints");
+var cors = require("cors");
+//Cors added
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.use(
-    bodyParser.urlencoded({
-      extended: true
-    })
-)  
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use('/api' , api);
+app.use("/api", api);
 
-app.listen(port, () => console.log(`The Hindu Scrapper is running at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`The Hindu Scrapper is running at http://localhost:${port}`)
+);
